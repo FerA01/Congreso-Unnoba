05793d2ce7f98cc804af91010b1fd2147acb6bf3
Fernando Andana <fernando_andana_@hotmail.com>
2022-10-31 01:06:30 -0300

    Primer commit

---
+++ src/main/java/com/ar/unnoba/congresos/CongresosApplication.java
@@ -0,0 +1,13 @@
+package com.ar.unnoba.congresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongresosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/unnoba/congresos/CongresosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.unnoba.congresos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
