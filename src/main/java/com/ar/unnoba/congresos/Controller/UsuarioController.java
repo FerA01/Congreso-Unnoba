0bfa57583f18ef1919d74d1cf7a5445d57adf8fc
Fernando Andana <fernando_andana_@hotmail.com>
2022-10-31 02:43:08 -0300

    Se agregaron clases modelo y un archivo html

---
+++ src/main/java/com/ar/unnoba/congresos/Controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.ar.unnoba.congresos.Controller;
+
+import com.ar.unnoba.congresos.Model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class UsuarioController {
+
+    @Autowired
+    public UsuarioController(){
+
+    }
+
+    @GetMapping("/test")
+    public String test(Model model){
+        model.addAttribute("usuario", new Usuario());
+        return "";
+    }
+}
---
+++ src/main/java/com/ar/unnoba/congresos/Model/Trabajo.java
@@ -0,0 +1,23 @@
+package com.ar.unnoba.congresos.Model;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "trabajos")
+public class Trabajo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date fecha_hora;
+    //private Usuario usuario;
+    private String estado;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Date getFecha_hora() { return fecha_hora; }
+    public void setFecha_hora(Date fecha_hora) { this.fecha_hora = fecha_hora; }
+    //public Usuario getUsuario() { return usuario; }
+    //public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+}
