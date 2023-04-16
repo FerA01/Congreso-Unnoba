7a01406e68bac497a583bb523958578c8afb733c
Fernando Andana <fernando_andana_@hotmail.com>
2023-04-15 22:03:01 -0300

    Agrego atributos a la vista acerca-de

--- src/main/java/com/ar/unnoba/congresos/Controller/AcercaDeController.java
+++ src/main/java/com/ar/unnoba/congresos/Controller/AcercaDeController.java
@@ -1,12 +1,23 @@
 package com.ar.unnoba.congresos.Controller;
+import com.ar.unnoba.congresos.Model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AcercaDeController {
     @GetMapping("/acerca-de")
-    public String acercaDe(){
-        return "/acercaDe";
+    public String acercaDe(Model model, Authentication auth){
+        User user = (User) auth.getPrincipal();
+        String role = "ROLE_USER";
+        model.addAttribute("id_user", user.getId());
+        if (auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
+            role = "ROLE_ADMIN";
+        }
+        model.addAttribute("role", role);
+        return "acercaDe";
     }
 }
 
